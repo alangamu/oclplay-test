7424b356eb8c6b7bf1089139a86105d533958d22
alangamu <alangamu@gmail.com>
2023-05-03 17:54:38 -0400

    timelines separation

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+    [SerializeField]
+    private IntVariable _dificultyLevel;
+    [SerializeField]
+    private Vector2 _movementSpeed;
+
+    private Vector2 _offset;
+    private Material _material;
+
+    private void Awake()
+    {
+        _material= GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        _offset = _dificultyLevel.Value * Time.deltaTime * _movementSpeed;
+        _material.mainTextureOffset += _offset;
+    }
+}
---
+++ Assets/Scripts/IntVariable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class IntVariable : ScriptableObject
+{
+    public int Value => _value;
+
+    [SerializeField]
+    [Range(1, 3)]
+    private int _value;
+
+    public void SetValue(int value)
+    {
+        _value = value;
+    }
+}
